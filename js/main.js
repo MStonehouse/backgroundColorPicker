bdb5ab29a15ea40bf132f4fe9bad891ae988b872
Mike Stonehouse <mikestonehouse@protonmail.com>
2020-10-03 18:07:13 -0700

    saving progress

--- js/main.js
+++ js/main.js
@@ -2,93 +2,105 @@
 
 
 
-function handleInputSubmit() {
-  let file = document.getElementById('image-input').files[0];
+function handleImageSubmit(event) {
+  let file = event.target.files[0];
+
   if (!file.type.startsWith('image/')) {return alert('file type is not an image')};
-  let preview = document.getElementById('preview');
-  let result = document.getElementById('result');
-  let resultText = document.getElementById('result-text');
 
-  while (preview.hasChildNodes()) {
-    preview.removeChild(preview.firstChild);
-  };
-
-  while (result.hasChildNodes()) {
-    result.removeChild(result.firstChild);
-  };
-
-  while (resultText.hasChildNodes()) {
-    resultText.removeChild(resultText.firstChild);
-  };
-
-  let img = document.createElement('img');
-  img.classList.add('obj');
-  let reader = new FileReader();
+  var img = document.createElement('img');
+  var reader = new FileReader();
   reader.onload = function(e) {img.src = e.target.result;}
   reader.readAsDataURL(file);
 
-  function proceed() {
-    let canvas = document.createElement('canvas');
-    let ctx = canvas.getContext('2d');
-    let rValue = [];
-    let gValue = [];
-    let bValue = [];
-    let sampleSize = 50; // 50
-    ctx.canvas.width = img.naturalWidth;
-    ctx.canvas.height = img.naturalHeight;
-    ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+  function proceed(width, height) {
+    var preview = document.getElementById('preview'); // get node for preview image
+    var result = document.getElementById('result'); // get node for result canvas
+    var resultText = document.getElementById('result-text'); // get node for result text
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var rgb; // object for rgb values
+    var rgbString; // rgb return string
+    var hexString; // hex return string
+    let sampleSize = 50; // number of pixels to sample
 
-    for (let i = 0; i < sampleSize; i++) {
-      let xPos = Math.floor(Math.random() * img.naturalWidth);
-      let yPos = Math.floor(Math.random() * img.naturalHeight);
-      let pixelData = ctx.getImageData(xPos, yPos, xPos+1, yPos+1);
-      rValue.push(pixelData.data[0]);
-      gValue.push(pixelData.data[1]);
-      bValue.push(pixelData.data[2]);
+    function clearNodes(...params) {
+      params.map((val) => { while (val.hasChildNodes()) { val.removeChild(val.firstChild) }})
     }
 
-    rValue = Math.floor(rValue.reduce((acc, val) => acc + val) / rValue.length);
-    gValue = Math.floor(gValue.reduce((acc, val) => acc + val) / gValue.length);
-    bValue = Math.floor(bValue.reduce((acc, val) => acc + val) / bValue.length);
+    function getColorValues(sampleSize) {
+      let r = [], g = [], b = [];
 
+      for (let i = 0; i < sampleSize; i++) {
+        let xPos = Math.floor(Math.random() * width);
+        let yPos = Math.floor(Math.random() * height);
+        let pixelData = ctx.getImageData(xPos, yPos, xPos+1, yPos+1);
+        r.push(pixelData.data[0]);
+        g.push(pixelData.data[1]);
+        b.push(pixelData.data[2]);
+      }
 
+      r = Math.floor(r.reduce((acc, val) => acc + val) / r.length);
+      g = Math.floor(g.reduce((acc, val) => acc + val) / g.length);
+      b = Math.floor(b.reduce((acc, val) => acc + val) / b.length);
 
+      return {r: r, g: g, b: b};
+    }
 
-    function componentToHex(c) {
-      var hex = c.toString(16);
+    function rgbToHex(value) {
+      var hex = value.toString(16);
       return hex.length == 1 ? "0" + hex : hex;
     }
 
-    function rgbToHex(r, g, b) {
-      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    function drawNewColorToCanvas() {
+      ctx.beginPath();
+      ctx.fillStyle = rgbString;
+      ctx.rect(0, 0, width, height);
+      ctx.fill();
     }
 
+    function drawImageToCanvas() {
+      ctx.canvas.width = width;
+      ctx.canvas.height = height;
+      ctx.drawImage(img, 0, 0, width, height);
+    }
 
-    let returnRGB = 'rgb(' + rValue.toString() + ', ' + gValue.toString() + ', ' + bValue.toString() + ')';
-    let returnHex = rgbToHex(rValue, gValue, bValue);
-    let rgbText = document.createElement('p');
-    let hexText = document.createElement('p');
-    rgbText.innerHTML = returnRGB;
-    hexText.innerHTML = returnHex;
-    //console.log(returnHex);
+    // set selected image to canvas so pixels can be selected off of it
+    drawImageToCanvas();
 
-    canvas.classList.add('obj');
-    ctx.beginPath();
-    ctx.fillStyle = returnRGB;
-    ctx.rect(0, 0, img.naturalWidth, img.naturalHeight);
-    ctx.fill();
+    // get color values from the image we just attached to canvas
+    rgb = getColorValues(sampleSize);
 
+    // make strings for rgb and hex to attach to DOM
+    rgbString = 'rgb(' + rgb.r.toString() + ', ' + rgb.g.toString() + ', ' + rgb.b.toString() + ')';
+    hexString = '#' + rgbToHex(rgb.r) + rgbToHex(rgb.g) + rgbToHex(rgb.b);
+
+    // recycle canvas, now that we have the color assign it to the same canvas we used to get the color
+    drawNewColorToCanvas();
+
+    // clear preview, result, and resultText divs
+    clearNodes(preview, result, resultText);
+
+    // make <p> elements to attach to resultText div
+    var rgbText = document.createElement('p');
+    var hexText = document.createElement('p');
+    rgbText.innerHTML = rgbString;
+    hexText.innerHTML = hexString;
+
+    // append all elements to their containers
     preview.appendChild(img);
     result.appendChild(canvas);
     resultText.appendChild(rgbText);
     resultText.appendChild(hexText);
   }
 
+
+
+  // set interval to check if file source has been attached to img variable yet
   var checkIfImgSrcSet = setInterval(function() {
-    if (img.src && img.naturalHeight != 0) {
+    if (img.src && img.naturalHeight) {
       console.log('image source is set, continuing with rest of program');
       clearInterval(checkIfImgSrcSet);
-      proceed();
+      proceed(img.naturalWidth, img.naturalHeight);
     } else {
       console.log('image source not set yet');
     }
